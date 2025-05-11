f6bfd250621dbf6b261542483afe602d45448ef0
maria <maria.dalva@aluno.uepb.edu.br>
2025-05-11 14:49:11 -0300

    fix

---
+++ backend/src/database/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from '../routes.js';
+import connectDataBase from './db.js';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(routes);
+
+
+connectDataBase()
+    .then(() => {
+        app.listen(6000, () => {
+            console.log('Servidor e banco de dados conectados');
+        });
+    })
+    .catch((error) => {
+        console.error('Erro ao conectar ao banco de dados:', error.message);
+        process.exit(1); // Encerra o processo se a conexão falhar
+    });
