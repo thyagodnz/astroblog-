fcf6e0bbebab805250fe23ab8d132422d0667734
Thyago Diniz <119258867+thyagodnz@users.noreply.github.com>
2025-05-11 22:28:11 -0300

    feat: integração back e front

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -2,10 +2,33 @@ import './login.css'
 import Header from '../../components/Header/header.jsx'
 import Footer from '../../components/Footer/footer.jsx'
 import { useNavigate } from 'react-router-dom'
+import { useRef } from 'react'
+import api from '../../services/api'
 
 function Login() {
-
     const navigate = useNavigate()
+
+    const inputEmail = useRef()
+    const inputPassword = useRef()
+
+    async function handleLogin() {
+        try {
+            const response = await api.post('/login', {
+                email: inputEmail.current.value,
+                password: inputPassword.current.value
+            })
+
+            if (response.status === 200) {
+                alert('Login realizado com sucesso!')
+
+                localStorage.setItem('user', JSON.stringify(response.data))
+                navigate('/')
+            }
+        } catch (error) {
+            alert('E-mail ou senha inválidos')
+            console.error(error)
+        }
+    }
 
     return (
         <>
@@ -13,9 +36,9 @@ function Login() {
             <div className='container'>
                 <form className='login'>
                     <h1>Fazer login</h1>
-                    <input placeholder='E-mail' name='email' type='email' />
-                    <input placeholder='Senha' name='senha' type='password' />
-                    <button type='button'>Login</button>
+                    <input placeholder='E-mail' name='email' type='email' ref={inputEmail} />
+                    <input placeholder='Senha' name='senha' type='password' ref={inputPassword} />
+                    <button type='button' onClick={handleLogin}>Login</button>
                     <h2 onClick={() => navigate('/forgot-password')}>
                         Esqueceu a senha?
                     </h2>
